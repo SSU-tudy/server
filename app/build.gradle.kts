e07799d30bb71c1d397aeda51733aa8722f1d429
rlawlgh0813 <jihooo7@naver.com>
2024-11-09 11:21:49 +0900

    merge kjh 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,9 +30,10 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.activity)
+    implementation(libs.constraintlayout)
     implementation(libs.appcompat)
     implementation(libs.material)
-    implementation(libs.activity)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
